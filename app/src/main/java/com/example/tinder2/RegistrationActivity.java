b4e3a36dd3ef5ddd9e61b65db7200a8e7cd66e28
gauravarya18 <37961028+gauravarya18@users.noreply.github.com>
2019-02-17 15:01:16 +0530

    Name and contact feature connected to Registration

--- app/src/main/java/com/example/tinder2/RegistrationActivity.java
+++ app/src/main/java/com/example/tinder2/RegistrationActivity.java
@@ -1,9 +1,9 @@
 package com.example.tinder2;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,12 +14,14 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegistrationActivity extends AppCompatActivity {
 
     private Button mRegister;
     private EditText mEmail, mPassword;
-
+    private EditText mContact,mName;
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener firebaseAuthStateListener;
 
@@ -48,16 +50,29 @@ public class RegistrationActivity extends AppCompatActivity {
         mEmail = (EditText) findViewById(R.id.email);
         mPassword = (EditText) findViewById(R.id.password);
 
+        mName= (EditText) findViewById(R.id.name);
+        mContact=(EditText) findViewById(R.id.contact);
+
         mRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 final String email = mEmail.getText().toString();
                 final String password = mPassword.getText().toString();
+                final String name=mName.getText().toString();
+                final String contact=mContact.getText().toString();
                 mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(RegistrationActivity.this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(!task.isSuccessful()){
                             Toast.makeText(RegistrationActivity.this, "sign up error", Toast.LENGTH_SHORT).show();
+                        }
+                        else
+                        {
+                            String userId=mAuth.getCurrentUser().getUid();
+                            DatabaseReference currentUserDb = FirebaseDatabase.getInstance().getReference().child("users").child(userId).child("name");
+                            currentUserDb.setValue(name);
+                            DatabaseReference currentUserDb1 = FirebaseDatabase.getInstance().getReference().child("users").child(userId).child("contact");
+                            currentUserDb1.setValue(contact);
                         }
                     }
                 });
