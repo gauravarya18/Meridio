9ab97e9df5c4939a7be592934e7e4c880e728d3a
gauravarya18 <37961028+gauravarya18@users.noreply.github.com>
2019-02-20 01:03:09 +0530

    Sub Activities Added and Profile Card's Layout Created

--- app/src/main/java/com/example/tinder2/ChooseTask.java
+++ app/src/main/java/com/example/tinder2/ChooseTask.java
@@ -1,13 +1,58 @@
 package com.example.tinder2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ChooseTask extends AppCompatActivity {
-
+    private Button mDashboard,mProfile,mPlay,mSettings;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_task);
+
+
+        mDashboard = (Button) findViewById(R.id.dashboard);
+        mDashboard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ChooseTask.this, DashboardActivity.class);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
+        mProfile = (Button) findViewById(R.id.profile);
+        mProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ChooseTask.this, ProfileActivity.class);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
+        mPlay = (Button) findViewById(R.id.play);
+        mPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ChooseTask.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
+        mSettings = (Button) findViewById(R.id.settings);
+        mSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ChooseTask.this, SettingsActivity.class);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
     }
 }
--- app/src/main/java/com/example/tinder2/LoginActivity.java
+++ app/src/main/java/com/example/tinder2/LoginActivity.java
@@ -34,7 +34,7 @@ public class LoginActivity extends AppCompatActivity {
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
                 if (user !=null){
-                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    Intent intent = new Intent(LoginActivity.this, ChooseTask.class);
                     startActivity(intent);
                     finish();
                     return;
